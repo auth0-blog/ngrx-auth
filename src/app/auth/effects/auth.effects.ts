067eeca98839d7ea0f574cbe905eda9bf7f1d915
Sam Julien <sam.julien@auth0.com>
2019-07-01 15:42:57 -0700

    Refactor effects to use createEffect

--- src/app/auth/effects/auth.effects.ts
+++ src/app/auth/effects/auth.effects.ts
@@ -1,108 +1,125 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { tap, exhaustMap, map, catchError } from 'rxjs/operators';
+import { Actions, Effect, ofType, createEffect } from '@ngrx/effects';
+import { tap, exhaustMap, map, catchError, mergeMap } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import * as fromAuth from '../actions/auth.actions';
 import { LogoutPromptComponent } from '@app/auth/components/logout-prompt.component';
 import { AuthService } from '@app/auth/services/auth.service';
-import { of, empty } from 'rxjs';
+import { of, EMPTY } from 'rxjs';
 
 @Injectable()
 export class AuthEffects {
-  @Effect({ dispatch: false })
-  login$ = this.actions$.pipe(
-    ofType<fromAuth.Login>(fromAuth.AuthActionTypes.Login),
-    tap(() => {
-      return this.authService.login();
-    })
+  login$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(fromAuth.AuthActionTypes.Login),
+        tap(() => {
+          return this.authService.login();
+        })
+      ),
+    { dispatch: false }
   );
 
-  @Effect()
-  loginComplete$ = this.actions$.pipe(
-    ofType<fromAuth.Login>(fromAuth.AuthActionTypes.LoginComplete),
-    exhaustMap(() => {
-      return this.authService.parseHash$().pipe(
-        map((authResult: any) => {
-          if (authResult && authResult.accessToken) {
-            this.authService.setAuth(authResult);
-            window.location.hash = '';
-            return new fromAuth.LoginSuccess();
-          }
-        }),
-        catchError(error => of(new fromAuth.LoginFailure(error)))
-      );
-    })
-  );
-
-  @Effect({ dispatch: false })
-  loginRedirect$ = this.actions$.pipe(
-    ofType<fromAuth.LoginSuccess>(fromAuth.AuthActionTypes.LoginSuccess),
-    tap(() => {
-      this.router.navigate([this.authService.authSuccessUrl]);
-    })
-  );
-
-  @Effect({ dispatch: false })
-  loginErrorRedirect$ = this.actions$.pipe(
-    ofType<fromAuth.LoginFailure>(fromAuth.AuthActionTypes.LoginFailure),
-    map(action => action.payload),
-    tap((err: any) => {
-      if (err.error_description) {
-        console.error(`Error: ${err.error_description}`);
-      } else {
-        console.error(`Error: ${JSON.stringify(err)}`);
-      }
-      this.router.navigate([this.authService.authFailureUrl]);
-    })
-  );
-
-  @Effect()
-  checkLogin$ = this.actions$.pipe(
-    ofType<fromAuth.CheckLogin>(fromAuth.AuthActionTypes.CheckLogin),
-    exhaustMap(() => {
-      if (this.authService.authenticated) {
-        return this.authService.checkSession$({}).pipe(
+  loginComplete$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fromAuth.AuthActionTypes.LoginComplete),
+      exhaustMap(() => {
+        return this.authService.parseHash$().pipe(
           map((authResult: any) => {
             if (authResult && authResult.accessToken) {
               this.authService.setAuth(authResult);
+              window.location.hash = '';
               return new fromAuth.LoginSuccess();
             }
           }),
-          catchError(error => {
-            this.authService.resetAuthFlag();
-            return of(new fromAuth.LoginFailure({ error }));
-          })
+          catchError(error => of(new fromAuth.LoginFailure(error)))
         );
-      } else {
-        return empty();
-      }
-    })
-  );
-
-  @Effect()
-  logoutConfirmation$ = this.actions$.pipe(
-    ofType<fromAuth.Logout>(fromAuth.AuthActionTypes.Logout),
-    exhaustMap(() =>
-      this.dialogService
-        .open(LogoutPromptComponent)
-        .afterClosed()
-        .pipe(
-          map(confirmed => {
-            if (confirmed) {
-              return new fromAuth.LogoutConfirmed();
-            } else {
-              return new fromAuth.LogoutCancelled();
-            }
-          })
-        )
+      })
     )
   );
 
-  @Effect({ dispatch: false })
-  logout$ = this.actions$.pipe(
-    ofType<fromAuth.LogoutConfirmed>(fromAuth.AuthActionTypes.LogoutConfirmed),
-    tap(() => this.authService.logout())
+  loginRedirect$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(fromAuth.AuthActionTypes.LoginSuccess),
+        tap(() => {
+          this.router.navigate([this.authService.authSuccessUrl]);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  loginErrorRedirect$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(fromAuth.AuthActionTypes.LoginFailure),
+        mergeMap(({ payload }) => payload),
+        tap((err: any) => {
+          if (err.error_description) {
+            console.error(`Error: ${err.error_description}`);
+          } else {
+            console.error(`Error: ${JSON.stringify(err)}`);
+          }
+          this.router.navigate([this.authService.authFailureUrl]);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  checkLogin$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fromAuth.AuthActionTypes.CheckLogin),
+      exhaustMap(() => {
+        if (this.authService.authenticated) {
+          return this.authService.checkSession$({}).pipe(
+            map((authResult: any) => {
+              if (authResult && authResult.accessToken) {
+                this.authService.setAuth(authResult);
+                return new fromAuth.LoginSuccess();
+              }
+            }),
+            catchError(error => {
+              this.authService.resetAuthFlag();
+              return of(new fromAuth.LoginFailure({ error }));
+            })
+          );
+        } else {
+          return EMPTY;
+        }
+      })
+    )
+  );
+
+  logoutConfirmation$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fromAuth.AuthActionTypes.Logout),
+      exhaustMap(() =>
+        this.dialogService
+          .open(LogoutPromptComponent)
+          .afterClosed()
+          .pipe(
+            map(confirmed => {
+              if (confirmed) {
+                return new fromAuth.LogoutConfirmed();
+              } else {
+                return new fromAuth.LogoutCancelled();
+              }
+            })
+          )
+      )
+    )
+  );
+
+  logout$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType<fromAuth.LogoutConfirmed>(
+          fromAuth.AuthActionTypes.LogoutConfirmed
+        ),
+        tap(() => this.authService.logout())
+      ),
+    { dispatch: false }
   );
 
   constructor(
